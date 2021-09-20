b8df7c8e7ba8395df293432846206c70b7ea9533
svenhornaff <sven.hornaff@gmail.com>
2021-09-20 16:45:09 +0200

    added coverage to testing and conftest with monkey

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -2,7 +2,7 @@ import pytest
 import webapp
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     app = webapp.create_app()
     app.debug = True
@@ -20,3 +20,14 @@ def test_some_id(app):
     res = app.get("/foo/12345")
     assert res.status_code == 200
     assert b"12345" in res.data
+
+
+@pytest.mark.xfail
+def test_divide_by_zero():
+    assert 1 / 0 == 1
+
+
+@pytest.mark.xfail
+def test_db_slap(db_conn):
+    db_conn.read_slaps()
+    assert ...
