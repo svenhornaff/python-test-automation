ba878bd55a4d966a0f3723b5d51fe75ea9eb275f
svenhornaff <sven.hornaff@gmail.com>
2021-09-20 20:48:27 +0200

    tox config added

--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, jsonify
 from flask_cors import CORS
 
--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,5 +1,5 @@
 import pytest
-import webapp
+from src.webapp import webapp
 
 
 @pytest.fixture(scope="session")
@@ -20,14 +20,3 @@ def test_some_id(app):
     res = app.get("/foo/12345")
     assert res.status_code == 200
     assert b"12345" in res.data
-
-
-@pytest.mark.xfail
-def test_divide_by_zero():
-    assert 1 / 0 == 1
-
-
-@pytest.mark.xfail
-def test_db_slap(db_conn):
-    db_conn.read_slaps()
-    assert ...
